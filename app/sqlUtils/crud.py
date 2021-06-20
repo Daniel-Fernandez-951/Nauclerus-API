1e21c336344b293330ca271b9050cbbe070447eb
DanielFernández <n231bb@gmail.com>
2021-06-19 17:32:11 -0700

    Update crud to working v1 input to db

--- app/sqlUtils/crud.py
+++ app/sqlUtils/crud.py
@@ -3,12 +3,17 @@ CRUD common function used to access the data from db
 """
 
 from sqlalchemy.orm import Session
-from . import models, schemas
+from sqlUtils import models, schemas
+
 
 # TODO: Add more functions GET/POST and update schemas
 # GET Functions
-def get_pilot_by_name(db: Session, user_name: int):
-    return db.query(models.Pilot).filter(models.Pilot.name == user_name).first()
+def get_pilot_by_name(db: Session, pilot_name: str):
+    return db.query(models.Pilot).filter(models.Pilot.name == pilot_name).first()
+
+
+def get_pilot_by_id(db: Session, pilot_id: int):
+    return db.query(models.Pilot).filter(models.Pilot.id == pilot_id).first()
 
 
 def get_flight_by_date(db: Session, flight_date: str):
@@ -20,7 +25,11 @@ def get_flight_by_year(db: Session, flight_year: int):
 
 
 def get_aircraft_by_tail(db: Session, tail_numb: str):
-    return db.query(models.Aircraft).filter(models.Aircraft.tail_num == tail_numb).all()
+    return db.query(models.Aircraft).filter(models.Aircraft.tail_num == tail_numb).first()
+
+
+def get_aircraft_by_id(db: Session, ac_id: int):
+    return db.query(models.Aircraft).filter(models.Aircraft.id == ac_id).first()
 
 
 # CREATE Functions
@@ -32,12 +41,16 @@ def create_flight(db: Session, flight: schemas.FlightCreate, pilot_id: int, airc
     return db_flight
 
 
-def create_aircraft(db: Session, aircraft: schemas.AircraftCreate):
-    pass
+def create_aircraft(db: Session, aircraft: schemas.AircraftCreate, pilot_id: int):
+    db_aircraft = models.Aircraft(**aircraft.dict(), pilot_id=pilot_id)
+    db.add(db_aircraft)
+    db.commit()
+    db.refresh(db_aircraft)
+    return db_aircraft
 
 
-def create_pilot(db: Session, pilot: schemas.PilotCreate, pilot_name: str):
-    db_pilot = models.Pilot(**pilot.dict(), name=pilot_name)
+def create_pilot(db: Session, pilot: schemas.PilotCreate):
+    db_pilot = models.Pilot(**pilot.dict())
     db.add(db_pilot)
     db.commit()
     db.refresh(db_pilot)
