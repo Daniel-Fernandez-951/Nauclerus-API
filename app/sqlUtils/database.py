bb999c67f97fff7cf919070e4b4a6ae448d98b36
DanielFernández <n231bb@gmail.com>
2021-10-01 02:30:53 -0700

    Add TRY block using two different methods of getting OS environment variable

--- app/sqlUtils/database.py
+++ app/sqlUtils/database.py
@@ -1,12 +1,15 @@
-from os import getenv
-from dotenv import load_dotenv
+from os import getenv, environ
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-load_dotenv()
+try:
+    SQLALCHEMY_DATABASE_URL = getenv('HEROKU_SQL_DB')
+    if SQLALCHEMY_DATABASE_URL is None:
+        SQLALCHEMY_DATABASE_URL = environ.get('HEROKU_SQL_DB')
+except KeyError:
+    raise KeyError("Set environment variable 'HEROKU_SQL_DB' to PostgreSQL URL")
 
-SQLALCHEMY_DATABASE_URL = getenv('HEROKU_SQL_DB')
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
