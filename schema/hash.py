4976f6d495ef7554b919f8876793cd06715656c5
DanielFernández <n231bb@gmail.com>
2021-12-14 04:28:14 -0800

    Hash functions to encrypt and decrypt token using `bcrypt`

---
+++ schema/hash.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+
+pwd_ctx = CryptContext(schemes=["bcrypt"],
+                       deprecated="auto")
+
+
+class Hash:
+    @staticmethod
+    def bcrypt(password: str):
+        return pwd_ctx.hash(password)
+
+    @staticmethod
+    def verify(hashed_password, plain_password):
+        return pwd_ctx.verify(plain_password, hashed_password)
