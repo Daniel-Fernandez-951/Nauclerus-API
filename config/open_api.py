f826898de31da6fa7c98e044753e2f727ae5bfa5
DanielFernández <n231bb@gmail.com>
2021-12-15 15:13:47 -0800

    Add Moesif API monitoring middleware-settings

--- config/open_api.py
+++ config/open_api.py
@@ -1,5 +1,8 @@
 
 # OpenAPI and Doc settings
+import os
+
+MOESIF_API_KEY = os.getenv('MOESIF_API')
 LOGO_PATH = "/app/logo/logo2-nauclerusAPIV1_dark.png"
 API_VERSION = "0.1.0"
 TAGS_METADATA = [
@@ -9,4 +12,8 @@ TAGS_METADATA = [
     {"name": "Flight", "description": "Flight POST and GET endpoints"},
     {"name": "Upload", "description": "Upload logbook file"}
 ]
-
+MOESIF_SETTINGS = {
+    'APPLICATION_ID': MOESIF_API_KEY,
+    'LOG_BODY': True,
+    'CAPTURE_OUTGOING_REQUESTS': True
+}
