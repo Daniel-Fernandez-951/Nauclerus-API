f256fdfadfe7c68474f9fe7a46ccdf2f5a3ad322
DanielFernández <n231bb@gmail.com>
2021-12-15 03:53:09 -0800

    Attempt to change openapi data

---
+++ config/open_api.py
@@ -0,0 +1,12 @@
+
+# OpenAPI and Doc settings
+LOGO_PATH = "/app/logo/logo2-nauclerusAPIV1_dark.png"
+API_VERSION = "0.1.0"
+TAGS_METADATA = [
+    {"name": "Pilot", "description": "Pilot POST and GET endpoints"},
+    {"name": "Aircraft", "description": "Aircraft POST and GET endpoints"},
+    {"name": "Logbook", "description": "Logbook POST and GET endpoints"},
+    {"name": "Flight", "description": "Flight POST and GET endpoints"},
+    {"name": "Upload", "description": "Upload logbook file"}
+]
+
