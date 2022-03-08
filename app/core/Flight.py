6f0f8f7748d75658aecf1040e4274d7d20333cdb
DanielFernández <dan@danielf.com>
2022-03-08 13:38:10 -0800

    Flake8: Remove unused imports

--- app/core/Flight.py
+++ app/core/Flight.py
@@ -1,11 +1,7 @@
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends
 
-from app.api import logbook_crud
 from app.database import configuration
 from app.schema.oa2 import get_current_user
-from app.schema.pilotSchema import PilotSecure
-from app.schema.logbookSchema import Logbook, LogbookCreate
 
 
 router = APIRouter(tags=["Logbook"],
