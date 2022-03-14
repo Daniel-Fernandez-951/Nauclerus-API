16b83e952134f5b69a762d8a10e3e70ea7c36886
DanielFernández <dan@danielf.com>
2022-03-14 16:49:31 -0700

    Increase API version; read `.env` values and check if both API left true

--- app/config/open_api.py
+++ app/config/open_api.py
@@ -6,9 +6,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Moesif settings
 MOESIF_API_KEY = os.getenv('MOESIF_API')
+# Elastic settings
+SERVICE_NAME = os.getenv('ELASTIC_SERVICE_NAME')
+SECRET_TOKEN = os.getenv('ELASTIC_SECRET_TOKEN')
+SERVER_URL = os.getenv('ELASTIC_SERVER_URL')
+ELASTIC_ENV = os.getenv('ELASTIC_ENV')
+
+
+MOESIF_ON = bool(int(os.getenv('MOESIF_ON')))
+ELASTIC_ON = bool(int(os.getenv('ELASTIC_ON')))
+# Check for API Mon. conflict
+if MOESIF_ON and ELASTIC_ON is True:
+    MOESIF_ON, ELASTIC_ON = False, False
+    print(f"MOE = {type(MOESIF_ON)}, {MOESIF_ON}\nELASTIC = {type(ELASTIC_ON)}, {ELASTIC_ON}")
+
+
 LOGO_PATH = "/app/logo/logo2-nauclerusAPIV1_dark.png"
-API_VERSION = "0.2.0"
+API_VERSION = "0.2.5"
 TAGS_METADATA = [
     {"name": "Pilot", "description": "Pilot POST and GET endpoints"},
     {"name": "Aircraft", "description": "Aircraft POST and GET endpoints"},
@@ -16,6 +32,7 @@ TAGS_METADATA = [
     {"name": "Flight", "description": "Flight POST and GET endpoints"},
     {"name": "Upload", "description": "Upload logbook file"}
 ]
+
 MOESIF_SETTINGS = {
     'APPLICATION_ID': MOESIF_API_KEY,
     'LOG_BODY': True,
